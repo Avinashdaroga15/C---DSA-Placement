827b182aa2e5401fbe1abe06e09b69beb0a11628
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-08-05 16:11:47 +0530

    fibbonachi program

--- 1_DecToBin.cpp
+++ 1_DecToBin.cpp
@@ -1,11 +1,24 @@
+//program to convert decimal number to binary number 
 #include<iostream>
+#include<math.h>
+
 using namespace std;
+
 int main(){
-    int num = 5,sum=0;
+    int num = 15,sum=0;
+    int i=0,bit,orgNum=num;
+    
     while (num!=0)
     {
-        
+        // bit = num&1;
+        // num = num>>1;
+        // sum = sum + bit* pow(10,i);
+        // i++;
+        bit = num%2;
+        num = num/2;
+        sum = sum + bit* pow(10,i);
+        i++;
     }
-    
-    
+    cout<<"Binary of "<<orgNum<<" is "<<sum<<endl;
+    return 0;
 }
