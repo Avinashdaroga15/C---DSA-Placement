4c1a2c4732cd6a35da8d77933837c8e8d3d9f80a
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-08-08 14:59:14 +0530

    upading logic in isNumber power of 2

--- 4_isNumberPower2.cpp
+++ 4_isNumberPower2.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 using namespace std;
 string isPowerOf2(int n){
-    int pow =1;
-    while (pow<=n)
-    {
-        pow*=2;
-        if(pow == n){
+    // // first logic
+    // int pow =1;
+    // while (pow<=n)
+    // {
+    //     pow*=2;
+    //     if(pow == n){
+    //         return "True";
+    //     }
+    // }
+    // return "False";
+
+    // second logic
+        if(!(n&1) || n==1){
             return "True";
         }
-    }
-    return "False";
+        return "False";
+
 }
 
 
