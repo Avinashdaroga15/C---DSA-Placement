ee76d0ebfa57fd3993a7ca55462cd0140cfa8dbb
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-08-08 13:47:18 +0530

    adding non-recursuve logic also

--- 2_fibbonachi.cpp
+++ 2_fibbonachi.cpp
@@ -2,14 +2,27 @@
 
 using namespace std;
 
-// Recursive fibonacci function
-int Fibonacci(int n) {
+// // Recursive fibonacci function
+// int Fibonacci(int n) {
 
-  // Base Case
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  // Recursive Case
-  return Fibonacci(n - 1) + Fibonacci(n - 2);
+//   // Base Case
+//   if (n == 0) return 0;
+//   if (n == 1) return 1;
+//   // Recursive Case
+//   return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+
+//non-Recursive fibonacci function
+int Fibonacci(int n) {
+int a=0,b=1,c;
+  while (n-1)
+  { 
+    c=a+b;
+    a=b;
+    b=c;
+    n--;
+  }
+  return b;
 }
 
 // main function
@@ -21,6 +34,6 @@ int main() {
   // Call fibonacci function in main and store its output in result
   result = Fibonacci(4);
   // Print value of result
-  cout << n << "th Fibonacci number = " << result;
+  cout << n << "th Fibonacci number = " << result<<endl;
   return 0;
 }
