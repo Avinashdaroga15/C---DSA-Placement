b776526601e604257c676b993e977e1afff4f37d
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-08-08 12:30:10 +0530

    note count program

---
+++ 5_NoteCount.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int amount,note,noteNum,count=1;
+    cout<<"Enter a number "<<endl;
+    cin>>amount;
+
+    while (amount != 0)
+    {
+        switch (count)
+        {
+        case 1: note=100;
+                count++;
+                break;
+        case 2: note=50;
+                count++;
+                break;
+        case 3: note=10;
+                count++;
+                break;
+        case 4: note=1;
+                count++;
+                break;
+        }
+
+        if (amount/note>0)
+        {
+            noteNum = amount/note;
+            if (note==1)
+            {
+                 amount/=10;
+            }else
+             amount%=note;
+            cout<<"Number of notes of "<<note<<"th note is "<<noteNum<<endl;
+        }
+        
+    }
+    return 0; 
+}
