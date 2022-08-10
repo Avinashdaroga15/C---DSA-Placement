c9e9ccf13f9df73562b7427160f2404e53058add
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-08-10 15:24:01 +0530

    adding better logic in unique element pro

--- 8_uniqueElem.cpp
+++ 8_uniqueElem.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
+void uniqueElement(int *arr,int size){
+  //logic 1 basic 
+  // for(int i=0;i<size-1;i++){
+  //   bool flag = true;
+  //   if(arr[i]!=0){
+  //     for(int j = i+1;j<size;j++){
+  //       if(arr[i]==arr[j]){
+  //         arr[j]=0;
+  //         flag=false;
+  //         break;
+  //       }
+  //     }
+  //     if(flag){
+  //       cout<<"unique element is "<<arr[i]<<endl;
+  //     }
+  //   }
+  // }
+//logic 2 better logic
+  int ans =0; 
+    for(int i=0;i<size;i++){
+        ans = ans^arr[i];
+      }
+      cout<<"unique element is "<<ans<<endl;
+}
+
 int main() {
-  int arr[9] = {2,3,2,8,4,3,5,5,8};
+  int arr[9] = {2,3,5,8,9,3,5,2,8};
   int size =9;
-  for(int i=0;i<size-1;i++){
-    bool flag = true;
-    if(arr[i]!=0){
-      for(int j = i+1;j<size;j++){
-        if(arr[i]==arr[j]){
-          arr[j]=0;
-          flag=false;
-          break;
-        }
-      }
-      if(flag){
-        cout<<"unique element is "<<arr[i]<<endl;
-      }
-    }
-  }
+  uniqueElement(arr,size);
   
 }
